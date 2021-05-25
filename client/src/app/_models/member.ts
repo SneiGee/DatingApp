ec5d2a2fc9444b358b5687a185ee0c3ac244d992
Michael Schneider <schneiderm312@gmail.com>
2021-05-25 10:18:58 +0100

    End of Section 9

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,18 @@
+import { Photo } from "./photo";
+
+export interface Member {
+    id: number;
+    username: string;
+    photoUrl: string;
+    age: number;
+    knowAs?: any;
+    created: Date;
+    lastActive: Date;
+    gender: string;
+    introduction: string;
+    lookingFor: string;
+    intersts?: any;
+    city: string;
+    country: string;
+    photos: Photo[];
+}
